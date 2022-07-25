2a65d4c56b1eb92f87d5c02c6f9a577058c267cc
Dexctersu <siuuopmta107defh@gmail.com>
2022-07-25 19:42:08 +0900

    カードを作成

--- Card.py
+++ Card.py
@@ -1,10 +1,29 @@
+from typing import List
+
 
 class Card:
-    
-    def __init__(self,marks,nos):
-        self.marks=marks
-        self.nos =nos
+
+    def __init__(self, marks: str, nos: str):
+        self.marks = marks
+        self.nos = nos
+
+    # カードを格納する
+    def create_decks(self) -> List[str]:
+        decks = []
+        for mark in self.marks:
+            for no in self.nos:
+                message = f'{mark}の{no}'
+                decks.append(message)
+        return decks
 
 
-c = Card(["ハート", "スペード", "ダイヤ", "クラブ"],[str(i) for i in range(1,14)])
-print(c)
+# nos = [str(i) for i in range(1, 14)]
+# nos[0] = "A"
+# nos[10] = "J"
+# nos[11] = "Q"
+# nos[12] = "K"
+# c = Card(["ハート", "スペード", "ダイヤ", "クラブ"], nos)
+
+# print(c.marks)
+# print(c.nos)
+# print(c.create_decks())

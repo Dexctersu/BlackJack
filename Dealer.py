e3937e5f811b1bca8c06e9d8188039e9f79abb6e
Dexctersu <siuuopmta107defh@gmail.com>
2022-07-29 21:41:50 +0900

    ディーラーのカードを引く処理の追加

--- Dealer.py
+++ Dealer.py
@@ -1,5 +1,14 @@
+from typing import List
 from PlayerBase import PlayerBase
 
 
 class Dealer(PlayerBase):
-    pass
+
+    def draw_card(self, card: object, dealer_score: List[str]):
+        total_score=self.calculate_total_score(dealer_score)
+        while total_score < 17:
+            drawn_card = card.draw_card()
+            dealer_score.append(drawn_card.split("の")[1])
+            print(f'ディーラーの引いたカードは{drawn_card}です')
+            total_score = self.calculate_total_score(dealer_score)
+        return self.calculate_total_score(dealer_score)

8e825547950aa031153282206f32bd230fa0c972
Dexctersu <siuuopmta107defh@gmail.com>
2022-07-30 19:19:23 +0900

    合計点を計算するメソッドを移行

--- PlayerBase.py
+++ PlayerBase.py
@@ -4,7 +4,15 @@ from typing import List
 
 class PlayerBase:
 
-    def __init__(self, hand: List[str]=[], score: int=0) -> None:
+    def __init__(self, hand: List[str] = [], score: int = 0) -> None:
         self.hand = hand
         self.score = score
 
+    def calculate_total_score(self, drawn_card: List[str]):
+        # ユーザーが引いたカードの合計点
+        total_score = 0
+        # ユーザーが引いたカードの枚数
+        n = len(drawn_card)
+        for i in range(n):
+            total_score += int(drawn_card[i])
+        return total_score

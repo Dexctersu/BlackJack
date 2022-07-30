cdad02643c840bdb54183dd36152a2800b844c56
Dexctersu <siuuopmta107defh@gmail.com>
2022-07-30 19:21:25 +0900

    コンフリクト解消

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import imp
 import Card
 from User import User
+from Dealer import Dealer
 
 
 def main():
@@ -8,17 +9,32 @@ def main():
     print("ゲームを開始します")
 
     user = User()
+    dealer = Dealer()
     card = Card.Card()
     # 引いたカードを出力
     user_score = []
+    dealer_score=[]
     for _ in range(2):
         drawn_card = card.draw_card()
         user_score.append(drawn_card.split("の")[1])
         print(f'あなたの引いたカードは{drawn_card}です')
-    print(f'ディーラーの引いたカードは{card.draw_card()}です')
+    for _ in range(2):
+        dealer_score.append(card.draw_card())
+    print(f'ディーラーの引いたカードは{dealer_score[0]}です')
     print(f'ディーラーの2枚目のカードは分かりません')
     print(f'あなたの現在の得点は{user.calculate_total_score(user_score)}です')
     print(user.draw_card(card, user_score))
+    print(f'ディーラーの2枚目のカードは{dealer_score[1]}でした')
+    dealer_score[0]=dealer_score[0].split("の")[1]
+    dealer_score[1]=dealer_score[1].split("の")[1]
+    print(f'ディーラーの現在の得点は{dealer.calculate_total_score(dealer_score)}です')
+    print(f'ディーラーの得点は{dealer.draw_card(card,dealer_score)}です')
+    print(f'あなたの得点は{user.calculate_total_score(user_score)}です')
+    if abs(21-user.calculate_total_score(user_score)) < abs(21-dealer.calculate_total_score(dealer_score)):
+        print("あなたの勝ちです")
+    else:
+        print("ディーラーの勝ちです")
+    print("ブラックジャック終了!!!")
     # print(f'残りの山札は{c.decks}')
 
 
--- User.py
+++ User.py
@@ -5,19 +5,7 @@ from PlayerBase import PlayerBase
 
 class User(PlayerBase):
 
-    # ユーザーが引いたカードの合計点を計算する
-
-    def calculate_total_score(self, drawn_card: List[str]):
-        # ユーザーが引いたカードの合計点
-        total_score = 0
-        # ユーザーが引いたカードの枚数
-        n = len(drawn_card)
-        for i in range(n):
-            total_score += int(drawn_card[i])
-        return total_score
-
-    def draw_card(self, card: object, user_score: List[str]) -> str:
-        error_message = ""
+    def draw_card(self, card: object, user_score: List[str]) -> None:
         while True:
             # 引いたカードを出力
             print('カードを引きますかカードを引く場合はYを引かない場合はNを入力してください')
@@ -25,10 +13,12 @@ class User(PlayerBase):
             if(s == "N"):
                 break
             if(s != "Y"):
-                error_message = "YとN以外が入力されました。強制終了します。"
+                print("YとN以外が入力されました。強制終了します。")
                 break
             drawn_card = card.draw_card()
             user_score.append(drawn_card.split("の")[1])
             print(f'あなたの引いたカードは{drawn_card}です')
             print(f'あなたの現在の得点は{self.calculate_total_score(user_score)}です')
-        return error_message
+            if(21 < self.calculate_total_score(user_score)):
+                print("21を超えました。あなたの負けです!!")
+                exit()

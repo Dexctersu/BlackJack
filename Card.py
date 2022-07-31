e445c1b11b1da485709e685741db3e4d798ad26f
Dexctersu <siuuopmta107defh@gmail.com>
2022-07-31 21:10:10 +0900

    ドキュメントコメントの追加

--- Card.py
+++ Card.py
@@ -6,8 +6,23 @@ import random
 
 class Card:
 
-    def __init__(self, mark, no) -> None:
+    def __init__(self, mark: str, no: int, used: bool=False) -> None:
+        """_summary_
+
+        Args:
+            mark (str): _description_
+            no (int): _description_
+            used (bool, optional): _description_. Defaults to False. 引いたカードか？ true:引いた false:引いていない
+        """
         def convert(number: int) -> str:
+            """_summary_
+            1,11,12,13を数値から文字列に変換する
+            Args:
+                number (int): _description_
+
+            Returns:
+                str: _description_
+            """
             if number == 1:
                 return "A"
             if number == 11:
@@ -22,7 +37,5 @@ class Card:
             raise ValueError
         self.mark = mark
         self.no = no
+        self.used =used
         self.card_name = f"{mark}の{convert(no)}"
-
-
-
--- User.py
+++ User.py
@@ -8,7 +8,13 @@ class User:
             hand = list()
         self.hand = hand
 
-    def calculate_total_score(self):
+    def calculate_total_score(self) -> int:
+        """_summary_
+        引いたカードの合計点を計算する
+
+        Returns:
+            _type_: _description_
+        """
         total_score = 0
         for i in self.hand:
             total_score += i.no

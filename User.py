11fb0da344153d5643a5fbeb5c0078eeabf09e7f
Dexctersu <siuuopmta107defh@gmail.com>
2022-07-31 21:05:25 +0900

    合計得点を計算する処理の追加

--- User.py
+++ User.py
@@ -1,24 +1,15 @@
 from typing import List
 from Card import Card
-from PlayerBase import PlayerBase
 
 
-class User(PlayerBase):
+class User:
+    def __init__(self, hand: List[object] = None):
+        if hand is None:
+            hand = list()
+        self.hand = hand
 
-    def draw_card(self, card: object, user_score: List[str]) -> None:
-        while True:
-            # 引いたカードを出力
-            print('カードを引きますかカードを引く場合はYを引かない場合はNを入力してください')
-            s = input()
-            if(s == "N"):
-                break
-            if(s != "Y"):
-                print("YとN以外が入力されました。強制終了します。")
-                break
-            drawn_card = card.draw_card()
-            user_score.append(drawn_card.split("の")[1])
-            print(f'あなたの引いたカードは{drawn_card}です')
-            print(f'あなたの現在の得点は{self.calculate_total_score(user_score)}です')
-            if(21 < self.calculate_total_score(user_score)):
-                print("21を超えました。あなたの負けです!!")
-                exit()
+    def calculate_total_score(self):
+        total_score = 0
+        for i in self.hand:
+            total_score += i.no
+        return total_score

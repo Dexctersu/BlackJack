0f95c3592cfb1b6482885a44da17e105d16431c9
Dexctersu <siuuopmta107defh@gmail.com>
2022-07-31 21:33:52 +0900

    コンストラクタの削除

---
+++ Deck.py
@@ -0,0 +1,25 @@
+from operator import truediv
+from typing import List
+from Card import Card
+import random
+
+
+class Deck:
+    def _initialize(self) -> List[int]:
+        marks = ["ハート", "スペード", "ダイヤ", "クローバー"]
+        self.cards = [Card(marks[i % 4], i % 13+1) for i in range(52)]
+        return self.cards
+
+    def draw(self,role:str,hide:bool=False) -> List[str]:
+        card = self._initialize()
+        r = random.randint(0, 51)
+        drawn_card=card[r]
+        while(True):
+            if(not(drawn_card.used)):
+                drawn_card.used=True
+
+                if(hide):
+                    print(f"{role}の引いたカードは分かりません")
+                else:
+                    print(f"{role}の引いたカードは{drawn_card.card_name}です")
+                return drawn_card

db64b9c47b069a45d18dd22ecb04920097aed5f4
Dexctersu <siuuopmta107defh@gmail.com>
2022-08-01 21:16:53 +0900

    メイン処理の追記

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
-import imp
 import Card
+from Deck import Deck
 from User import User
 from Dealer import Dealer
 
@@ -10,33 +10,53 @@ def main():
 
     user = User()
     dealer = Dealer()
-    card = Card.Card()
-    # 引いたカードを出力
-    user_score = []
-    dealer_score=[]
-    for _ in range(2):
-        drawn_card = card.draw_card()
-        user_score.append(drawn_card.split("の")[1])
-        print(f'あなたの引いたカードは{drawn_card}です')
-    for _ in range(2):
-        dealer_score.append(card.draw_card())
-    print(f'ディーラーの引いたカードは{dealer_score[0]}です')
-    print(f'ディーラーの2枚目のカードは分かりません')
-    print(f'あなたの現在の得点は{user.calculate_total_score(user_score)}です')
-    print(user.draw_card(card, user_score))
-    print(f'ディーラーの2枚目のカードは{dealer_score[1]}でした')
-    dealer_score[0]=dealer_score[0].split("の")[1]
-    dealer_score[1]=dealer_score[1].split("の")[1]
-    print(f'ディーラーの現在の得点は{dealer.calculate_total_score(dealer_score)}です')
-    print(f'ディーラーの得点は{dealer.draw_card(card,dealer_score)}です')
-    print(f'あなたの得点は{user.calculate_total_score(user_score)}です')
-    if abs(21-user.calculate_total_score(user_score)) < abs(21-dealer.calculate_total_score(dealer_score)):
+    deck = Deck()
+
+    # プレイヤーが最初のカードを2枚引く
+    user.hand.append(deck.draw("あなた"))
+    user.hand.append(deck.draw("あなた"))
+
+    # ディーラーがカードを2枚引く
+    dealer.hand.append(deck.draw("ディーラー"))
+    dealer.hand.append(deck.draw("ディーラー", hide=True))
+
+    print(f"あなたの現在の得点は{user.calculate_total_score()}です")
+
+    # ユーザーのターン
+    while(True):
+        print("カードを引きますか? 引く場合はYを、引かない場合はNを入力してください。")
+        s = input()
+        if(s == "Y" or s == "y"):
+
+            user.hand.append(deck.draw("あなた"))
+
+            print(f"あなたの現在の得点は{user.calculate_total_score()}です")
+
+            if(user.is_burst()):
+                print("21を超えました")
+                print("あなたの負けです")
+                exit()
+
+        elif(s == "N" or s == "n"):
+            break
+        else:
+            print("YかNを入力してください。")
+
+    print(f"ディーラーの2枚目のカードは{dealer.hand[1].card_name}でした")
+
+    print(f"ディーラーの現在の得点は{dealer.calculate_total_score()}です")
+
+    while dealer.calculate_total_score() < 17:
+        dealer.hand.append(deck.draw("ディーラー"))
+
+    if dealer.is_burst() or abs(21-user.calculate_total_score())< abs(21-dealer.calculate_total_score()):
         print("あなたの勝ちです")
+    elif user.calculate_total_score() == dealer.calculate_total_score():
+        print("引き分けです")
     else:
         print("ディーラーの勝ちです")
-    print("ブラックジャック終了!!!")
-    # print(f'残りの山札は{c.decks}')
 
+    print("ブラックジャック終了")
 
 if __name__ == '__main__':
     main()
